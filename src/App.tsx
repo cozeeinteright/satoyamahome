16d920ad5ee8edb1f6dff3206d4f9486d253cf11
koji noguchi <info@interight.co.jp>
2022-08-03 07:07:49 +0000

    add cli

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
   return (
     <div>
-      <div>Hello World!</div>
+      <div>{data}</div>
+      <div>satoyama</div>
     </div>
-  );
+  )
 }
 
 export default App;
