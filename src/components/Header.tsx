62c24a3ea7fcfca17c0de0ae90ac0f344e873ce9
koji noguchi <info@interight.co.jp>
2022-08-04 09:36:43 +0000

    edit Header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Toolbar ,Typography} from '@mui/material'
 import AdUnitsIcon from '@mui/icons-material/AdUnits';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
   return (
@@ -9,8 +10,9 @@ export default function Header() {
         <AdUnitsIcon fontSize="large" sx={{mr:2}}/>
         <Typography variant="body1" sx={{flexGrow: 1}}>里山生物多様性プロジェクト</Typography>
         <GitHubIcon fontSize="large" sx={{mx:1, border: 1, borderRaduios: 2, borderColor: 'primary.main'}} />
+        <MenuIcon fontSize="large" sx={{display:{xs:'block',sm:'none'}, mr:2}}/>
       </Toolbar>
-      <Toolbar sx={{display:'flex', justifyContent: 'space-evenly'}}>
+      <Toolbar sx={{display:{xs:'none',sm:'flex'},justifyContent: 'space-evenly'}}>
         <Typography variant="body1">ドキュメント</Typography>
         <Typography variant="body1">寄附</Typography>
         <Typography variant="body1">活動</Typography>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@mui/material'
 import Header from './Header'
-import UserName from "./UserName";
+import UserName from './UserName'
+import FirstView from './FirstView'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <div>Here is Home</div>
         <UserName />
         <Header />
+        <FirstView />
       </Container>
     </>
   );
