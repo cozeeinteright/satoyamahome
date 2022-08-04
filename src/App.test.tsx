62701a4f44b8f9d370b9b3b531b74f667077f592
koji noguchi <info@interight.co.jp>
2022-08-04 08:13:33 +0000

    add wapper to App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import {BrowserRouter} from 'react-router-dom'
+
 import App from "./App";
 
 test("renders Hello world text", () => {
-  render(<App />);
+  render(<App />, {wrapper: BrowserRouter});
   const Hello = screen.getByText(/Satoyama Home Page/i);
   expect(Hello).toBeInTheDocument();
 });
