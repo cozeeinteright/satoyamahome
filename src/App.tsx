04322c6e148dde42e7ebc8414fd35f6bfeba5e4a
koji noguchi <info@interight.co.jp>
2022-08-04 06:33:08 +0000

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './components/Home'
+import SignIn from './components/SignIn'
 
-function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    (async function () {
-      try {
-        const { name } = await( await fetch(`/api/message`)).json();
-        console.log(name)
-        setData(name)
-        console.log("fetch succes")
-      } catch(err) {
-        console.log("fetch error")
-        console.log(err)
-      }
-    })();
-  });
-
+export default function App() {
   return (
     <>
-      <div>satoyama</div>
-      <div>{data}</div>;
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </>
-  )
+  );
 }
-
-export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import UserName from './UserName'
+
+export default function Home() {
+  return (
+    <>
+      <div>Here is Home</div>
+      <UserName />
+    </>
+  )
+}
